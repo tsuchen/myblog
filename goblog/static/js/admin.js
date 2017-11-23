d25fc0dc663b1a4f13c4b9ef143d2421c5a0bdd2
tsuchen <320643506@qq.com>
2017-11-23 23:08:17 +0800

    修改后台页面

--- goblog/static/js/admin.js
+++ goblog/static/js/admin.js
@@ -6,9 +6,32 @@
 //dom init finish
 $(function(){
   //加载menu
-
+  hideMenuList()
 });
 
 /**
- *  drop menu
+ *  隐藏menu子菜单
  */
+function hideMenuList(){
+  $("#articleListGroup").hide(0);
+  $("userListGroup").hide(0);
+};
+
+$("#articleButton").click(function () {
+  var div = $("#articleListGroup");
+  if(div.is(":visible")){
+    div.hide(500);
+  }else{
+    div.show("slow");
+  }
+});
+
+$("#userButton").click(function () {
+  var node = $("#userListGroup");
+  if(node.is(":visible")){
+    node.hide(500);
+  }else{
+    node.show("slow");
+  }
+});
+
