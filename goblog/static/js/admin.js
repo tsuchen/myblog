feccab3d03d19faf13f8b90765afb5820118cc91
tsuchen <320643506@qq.com>
2017-11-25 17:01:35 +0800

    修改后台管理页面

--- goblog/static/js/admin.js
+++ goblog/static/js/admin.js
@@ -13,25 +13,21 @@ $(function(){
  *  隐藏menu子菜单
  */
 function hideMenuList(){
-  $("#articleListGroup").hide(0);
-  $("userListGroup").hide(0);
+  $(".menu-list-group").hide(0);
 };
 
-$("#articleButton").click(function () {
-  var div = $("#articleListGroup");
-  if(div.is(":visible")){
-    div.hide(500);
-  }else{
-    div.show("slow");
-  }
-});
-
-$("#userButton").click(function () {
-  var node = $("#userListGroup");
-  if(node.is(":visible")){
-    node.hide(500);
-  }else{
-    node.show("slow");
-  }
-});
-
+/**
+ * 添加菜单事件
+ */
+var menuButtons = $($(".menu-button"));
+for(let index = 0; index < menuButtons.length; index++){
+  var menuButton = $(menuButtons[index])
+  menuButton.click(function(){
+    var listGrounp = $($(".menu-list-group")[index]);
+    if (listGrounp.is(":visible")){
+      listGrounp.hide(300)
+    }else{
+      listGrounp.show(300);
+    }
+  });
+};
